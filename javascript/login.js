8bcca92abbe6f43151300dfad0edaff8f62c95d2
AbdelrahmanAli <abdelrahmanali2310@gmail.com>
2025-08-17 23:05:13 +0300

    end of validate of log in

---
+++ javascript/login.js
@@ -0,0 +1,25 @@
+    document.getElementById("loginForm").addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const username = document.getElementById("username").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      let users = JSON.parse(localStorage.getItem("users")) || [];
+
+      const user = users.find(u => u.username === username && u.password === password);
+
+      if (!user) {
+        alert("Invalid username or password!");
+        return;
+      }
+
+      if (user.role === "Seller") {
+        window.location.href = "pages/seller/pages/index.html";
+      } else if (user.role === "Buyer") {
+        window.location.href = "pages/buyer/pages/index.html";
+      }else if (user.role === "Admin") {
+        window.location.href = "pages/admin/pages/index.html";
+      } else {
+        alert("Unknown role! Please register again.");
+      }
+    });
